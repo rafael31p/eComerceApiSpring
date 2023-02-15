55e1020da24ef7015a2205262df8abab6de22b91
Rafael Peña <rpena@heinsohn.com.co>
2023-02-15 10:21:12 -0500

    se genera controlador

--- src/main/java/com/example/EComerceRafaelSama/HiHiWilita.java
+++ src/main/java/com/example/EComerceRafaelSama/HiHiWilita.java
@@ -1,4 +1,14 @@
 package com.example.EComerceRafaelSama;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
 public class HiHiWilita {
+    @GetMapping("/hihi")
+    public String Hi(){
+        return "Hola, Wilita <3";
+    }
 }
