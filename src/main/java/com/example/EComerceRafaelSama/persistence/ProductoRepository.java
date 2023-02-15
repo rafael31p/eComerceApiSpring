e05918b0801c1334587935a79a3cb3797ef0e003
Rafael Peña <rpena@heinsohn.com.co>
2023-02-15 17:59:05 -0500

    Se esta configurando los repository del dominio

--- src/main/java/com/example/EComerceRafaelSama/persistence/ProductoRepository.java
+++ src/main/java/com/example/EComerceRafaelSama/persistence/ProductoRepository.java
@@ -1,4 +1,7 @@
 package com.example.EComerceRafaelSama.persistence;
+import com.example.EComerceRafaelSama.domain.Product;
+import com.example.EComerceRafaelSama.domain.repository.productRepository;
+import com.example.EComerceRafaelSama.persistence.Mapper.ProductMapper;
 import com.example.EComerceRafaelSama.persistence.entity.producto;
 import com.example.EComerceRafaelSama.persistence.crud.ProductoCrudRepository;
 import org.springframework.stereotype.Repository;
@@ -7,11 +10,35 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements productRepository {
     private ProductoCrudRepository productoCrudRepository;
-    public List<producto> getAll(){
-        return (List<producto>) productoCrudRepository.findAll();
+
+    private ProductMapper mapper;
+    public List<Product> getAll(){
+        List<producto> productos= (List<producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
     }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProduct(int quantity) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return null;
+    }
+
     public List<producto> getByCategoria(int idCategoria){
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
     }
