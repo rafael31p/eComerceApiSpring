c7a8f1d3ee29411358c340d1d62646eb15523dff
rafael31p <rafael31p@gmail.com>
2023-05-06 16:02:03 -0500

    Se generaron los servicios de purchase

---
+++ src/main/java/com/example/eComerceApiSpring/webController/PurchaseController.java
@@ -0,0 +1,37 @@
+package com.example.eComerceApiSpring.webController;
+
+import com.example.eComerceApiSpring.domain.Purchase;
+import com.example.eComerceApiSpring.domain.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+    @Autowired
+    private final PurchaseService purchaseService;
+
+    public PurchaseController(PurchaseService purchaseService) {
+        this.purchaseService = purchaseService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Purchase>> getAll(){
+        return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/client/{idClient}")
+    public ResponseEntity<List<Purchase>> getByClient(@PathVariable String idClient){
+        return purchaseService.getByClient(idClient).map(purchases ->
+                new ResponseEntity<>(purchases, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase){
+        return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
+    }
+}
